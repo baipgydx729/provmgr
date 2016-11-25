0c0845f926844d00c538065c705438c06087ccf7
yuwenzhong <yuwenzhong@new4g.local>
2016-11-25 17:41:01 +0800

    建设银行报表开发

---
+++ src/main/java/com/qdb/dao/model/DataTable3Entity.java
@@ -0,0 +1,46 @@
+package com.qdb.dao.model;
+
+import java.util.List;
+
+/**
+ * 表3行对象
+ * Created by yuwenzhong on 2016-11-24.
+ */
+public class DataTable3Entity {
+    private String bankName;
+    private String accountName;
+    private String accoutNo;
+    private List<DataTable1_3> list;
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getAccoutNo() {
+        return accoutNo;
+    }
+
+    public void setAccoutNo(String accoutNo) {
+        this.accoutNo = accoutNo;
+    }
+
+    public List<DataTable1_3> getList() {
+        return list;
+    }
+
+    public void setList(List<DataTable1_3> list) {
+        this.list = list;
+    }
+}
---
+++ src/main/java/com/qdb/service/ccb/CBCReportService.java
@@ -0,0 +1,62 @@
+package com.qdb.service.ccb;
+
+import com.qdb.dao.DBUtil;
+import com.qdb.dao.TableModeEnum;
+import com.qdb.dao.ccb.CCBReportDao;
+import com.qdb.dao.model.DataTable1_3;
+import com.qdb.dao.model.DataTable3Entity;
+import com.qdb.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+/**
+ * Created by yuwenzhong on 2016-11-23.
+ */
+@Service
+public class CBCReportService {
+
+    @Autowired
+    private CCBReportDao ccbReportDao;
+
+    /**
+     * 查询表1、2、3、6、9、10数据
+     * @param tableType
+     * @param bankName
+     * @param accountNo
+     * @param beginDate
+     * @param endDate
+     * @return
+     */
+    public List<Map<String, Object>> findTableDataList(String tableType, String bankName, String accountNo, String beginDate, String endDate){
+        List<Map<String, Object>> dataList = ccbReportDao.queryDataList(tableType, bankName, accountNo, beginDate, endDate);
+        return dataList;
+    }
+
+
+    /**
+     * 针对表3的数据，封装单行数据对象
+     * @param list
+     * @return
+     */
+    public DataTable3Entity convertResult2Entity(List<Map<String, Object>> list){
+        DataTable3Entity rowEntity13 = new DataTable3Entity();
+        List<DataTable1_3> rowC01List = new LinkedList<>();
+        int days = list.size();
+        for (int i = 0; i < days; i++) {
+            DataTable1_3 dataTable3 = new DataTable1_3();
+
+            Map<String, Object> map = list.get(i);
+            dataTable3.setC01((Double) map.get("C01"));
+            rowC01List.add(dataTable3);
+        }
+        Map<String, Object> map = list.get(0);
+        rowEntity13.setBankName((String) map.get("bankName_S"));
+        rowEntity13.setAccoutNo((String) map.get("AD"));
+        rowEntity13.setAccountName((String) map.get("name"));
+        rowEntity13.setList(rowC01List);
+        return rowEntity13;
+    }
+
+}
