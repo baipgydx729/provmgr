a7d5a6490e85b227c509610b2593d95ca4fec16a
wenzhong <wenzhong@WENZHONG-MBP.local>
2016-12-02 12:25:02 +0800

    修改导出功能

--- src/main/java/com/qdb/provmgr/controller/report/CcbReportController.java
+++ src/main/java/com/qdb/provmgr/controller/report/CcbReportController.java
@@ -1,7 +1,6 @@
 package com.qdb.provmgr.controller.report;
 
 import com.qdb.provmgr.common.TableNameConstant;
-import com.qdb.provmgr.dao.entity.report.DataTable1_3;
 import com.qdb.provmgr.dao.entity.report.DataTable3Entity;
 import com.qdb.provmgr.service.ccb.CCBReportService;
 import com.qdb.provmgr.util.ExcelUtils;
@@ -10,17 +9,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yuwenzhong on 2016-11-28.
  */
-@RequestMapping("/ccb")
+@RequestMapping("/report/ccb")
 @Controller
 public class CcbReportController {
 
@@ -43,45 +43,34 @@ public class CcbReportController {
     public void ctrateReports(HttpServletRequest request, HttpServletResponse response){
         String beginDate = "2016-10-01";
         String endDate = "2016-10-31";
-        String tableType = "1_6";
+        String tableType = "1_10";
 //        for (String tableType: typeList) {
         String tableNo = tableNameMap.get(tableType);
-            String templateName = "agentCode_" + tableNo + "_n.xls";
+        String templateName = "agentCode_" + tableNo + "_n.xls";
 
-            String agentCode = "20161130";
-            String destFileName = agentCode+"_" + tableNo + "_0.xls";
+        String agentCode = "20161130";
+        String destFileName = agentCode+"_" + tableNo + "_0.xls";
 
-            String bankType = "ccb";
-            Map<String, Object> infoMap = new HashMap<>();
-            infoMap.put("writeTable", writetable);
-            infoMap.put("checkTable", checktable);
+        String bankType = "ccb";
+        Map<String, Object> infoMap = new HashMap<>();
+        infoMap.put("writeTable", writetable);
+        infoMap.put("checkTable", checktable);
 
-            String sheetName = "sheet";
-            if("1_3".equals(tableType)){
-                List<Map<String, Object>> dataList = reportService.findTableDataList(tableType, BANKNAME, "汇总", "99999", null, beginDate, endDate);
-                infoMap.put("total", dataList);
+        String sheetName = "sheet";
+        if("1_3".equals(tableType)){
+            List<Map<String, Object>> dataList = reportService.findTableDataList(tableType, BANKNAME, "汇总", "99999", null, beginDate, endDate);
+            infoMap.put("total", dataList);
 //                infoMap.put("dates", dayList);
-                List<DataTable3Entity> table3DataList = reportService.findTable3Data(tableType, BANKNAME, null, null, beginDate, endDate);
-                ExcelUtils.excelDownLoad(request, response, table3DataList, sheetName, bankType, templateName, destFileName, infoMap);
-            }else{
-                //建行按照客户级别上报，不分账户，统计每天合计金额
-                List<Map<String, Object>> dataList = reportService.findTableDataList(tableType, BANKNAME, "汇总", "99999", null, beginDate, endDate);
-                ExcelUtils.excelDownLoad(request, response, dataList, sheetName, bankType, templateName, destFileName,infoMap);
-            }
+            List<DataTable3Entity> table3DataList = reportService.findTable3Data(tableType, BANKNAME, null, null, beginDate, endDate);
+            ExcelUtils.excelDownLoad(request, response, table3DataList, sheetName, bankType, templateName, destFileName, infoMap);
+        }else{
+            //建行按照客户级别上报，不分账户，统计每天合计金额
+            List<Map<String, Object>> dataList = reportService.findTableDataList(tableType, BANKNAME, "汇总", "99999", null, beginDate, endDate);
+            ExcelUtils.createExcel(request, response, dataList, sheetName, bankType, templateName, destFileName,infoMap);
+        }
 //        }
     }
 
-    /**
-     * 返回日列表
-     * @param days
-     * @return
-     */
-    private List<Integer> getDayList(int days) {
-        List<Integer> dayList = new ArrayList<>();
-        int i = 1;
-        while (i <= days){
-           dayList.add(i);
-        }
-        return dayList;
-    }
+
+
 }
