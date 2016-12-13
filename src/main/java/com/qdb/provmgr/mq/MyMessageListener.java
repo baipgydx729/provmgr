a2bbe59cbadd6b4798f6dea5dbf12e402e1c0f8f
mashengli <mslvictor@163.com>
2016-12-13 17:01:43 +0800

    Fix mq

--- src/main/java/com/qdb/provmgr/mq/MyMessageListener.java
+++ src/main/java/com/qdb/provmgr/mq/MyMessageListener.java
@@ -1,16 +1,16 @@
-package com.qdb.provmgr.mq;
-
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
-import org.springframework.stereotype.Component;
-
-/**
- * @author mashengli
- */
-@Component
-public class MyMessageListener implements MessageListener {
-    @Override
-    public void onMessage(Message message) {
-
-    }
-}
+//package com.qdb.provmgr.mq;
+//
+//import org.springframework.amqp.core.Message;
+//import org.springframework.amqp.core.MessageListener;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author mashengli
+// */
+//@Component
+//public class MyMessageListener implements MessageListener {
+//    @Override
+//    public void onMessage(Message message) {
+//
+//    }
+//}
